afb7ce12158fe1196d2bbbd22402f5fc4d9eddbb
Hardik Rathore <hardikrathore1009@gmail.com>
2026-02-13 20:01:10 +0530

    fix: correct import statement formatting and remove unused RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Counter } from "./components/counter/counter";
+import { Counter } from './components/counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Counter],
+  imports: [Counter],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('counter-app');
